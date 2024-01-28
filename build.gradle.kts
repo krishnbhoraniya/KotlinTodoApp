461715623a9faeed688dc2bba0357b036b75c8e1
Krishn <kishan.bhoraniya1998@gmail.com>
2024-01-28 17:12:43 +0530

    - Added kapt dependency and other required dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,12 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
+}
+
+buildscript {
+    dependencies {
+        classpath(libs.kotlin.gradlePlugin)
+    }
 }
